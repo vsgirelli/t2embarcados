e2d7bea46598c53b93b96e84461b546892f4d982
Valeria Girelli <vsgirelli@gppd-hpc.inf.ufrgs.br>
2019-06-14 21:02:48 -0300

    Correct results

--- NPB3.4-OMP/execs/run.sh
+++ NPB3.4-OMP/execs/run.sh
@@ -4,6 +4,8 @@ cd slurm
 rm -f *
 cd ../
 
-for class in A B C; do
-  sbatch --error="slurm/%j_is.$class.err" --output="slurm/%j_is.$class.out" -J "emb.is.$class" run.slurm is $class
+for rep in {1..30}; do
+  for class in A B C; do
+    sbatch --error="slurm/%j_is.$class.err" --output="slurm/%j_is.$class.out" -J "emb.is.$class" run.slurm is $class
+  done
 done
